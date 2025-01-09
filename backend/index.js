32d2a7a82b449b717bb71b687b137bfaa3b0c7a0
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-01-09 22:39:26 +0500

    SHARJEEL

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const PORT = 5000;
+// routes
+app.get('/', (req , res)=>{
+    res.send("hello world")
+})
+app.listen(PORT ,  ()=>{
+    console.log('server is running' ,PORT);
+    
+})
+// some things went wrong form my code 
