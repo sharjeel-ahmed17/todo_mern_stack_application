192cbac55366100a0663963d7f56e81dee9e06f3
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-01-09 22:24:35 +0500

    mongo db connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 import express from "express";
+import cors from "cors";
+import mongoose, { mongo } from "mongoose";
 const app = express();
 const PORT = 5000;
+
+// middleware
+app.use(express.json())
+// app.use(express.urlencoded(true))
+app.use(cors('*'))
+
+
+// db connection
+
+mongoose.connect('mongodb+srv://kifito3954:Y8UP20Dc4aVEtyKw@blood-donor.e4vij.mongodb.net/').then(()=>{
+    console.log('db connected');
+}).catch((err)=>{
+    console.log(err);
+ 
+})
 // routes
 app.get('/', (req , res)=>{
     res.send("hello world")
